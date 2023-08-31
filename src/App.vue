1e99c77c9c351cd89117dcf90cc6dde647d3a09c
wupeng <wupeng_s@163.com>
2023-08-31 23:15:28 +0800

    增加store，按模块区分

--- src/App.vue
+++ src/App.vue
@@ -3,30 +3,32 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <div>{{ userInfo.userName }}</div>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
+<script>
+import { mapState } from "vuex";
+export default {
+  name: "App",
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapState({
+      userInfo: (state) => state.user.userInfo,
+    }),
+  },
+};
+</script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
 }
 </style>
---
+++ src/store/modules/user.js
@@ -0,0 +1,14 @@
+// 用户信息
+const user = {
+  namespaced: true,
+  state: {
+    userInfo: {
+      userName: "admin",
+      password: 123456,
+    },
+  },
+  mutations: {},
+  actions: {},
+};
+
+export default user;
